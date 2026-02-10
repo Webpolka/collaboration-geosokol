1476075acb25b88d7b82014f0cb69665bacf2cb3
Webpolka <webpolka@gmail.com>
2026-02-10 09:24:02 +0300

    10.02.2026 9:22

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,9 @@ import { BaseHelpers } from "./helpers/base-helpers";
 BaseHelpers.addLoadedClass();
 BaseHelpers.addTouchClass();
 
+// import autoREM from './helpers/auto-rem';
+// autoREM(1440, 16);
+
 /* -------------------------------------------------------------------------------------------------------------------------------------------------
 Force Repaint code for fix Chrome GPU blur bag
 -----------------------------------------------------------------------------------------------------------------------------------------------------*/
